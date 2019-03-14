61fb14776b52053beed9c4afdeceee58e3b919ac
SandeepBaskaran <sandeepbaskaran98@gmail.com>
2019-03-14 15:07:24 +0530

    14 March 2019

--- script.py
+++ script.py
@@ -127,7 +127,31 @@ if __name__ == "__main__":
 
 
 
+"""
+#import modules
+import time
+import RPi.GPIO as GPIO
 
+#GPIO setup
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+#main loop
+try:
+    while 1:
+        tempStore = open("/sys/bus/w1/devices/28-03119779c0c6/w1_slave")    #change this number to the Device ID of your sensor
+        data = tempStore.read()
+        tempStore.close()
+        tempData = data.split("\n")[1].split(" ")[9]
+        temperature = float(tempData[2:])
+        temperature = temperature/1000
+        print temperature
+
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    GPIO.cleanup()
+    print ("Program Exited Cleanly")
+"""
 
 
 
